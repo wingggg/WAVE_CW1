ae9bcd016cacb68a1c3643a77e190700a43e069a
unknown <wsn12@eews506a-032.ic.ac.uk>
2016-03-27 23:52:37 +0100

    Q4 in progress

--- project_files_&_data/Q4.m
+++ project_files_&_data/Q4.m
@@ -1,14 +1,27 @@
+% Q1
+
 n_samp = 2048;
 T = 64;
 K = 2;
 
 % orig_t_k = floor(rand([2,1])*n_samp/T)*T;
 % orig_a_k = floor(rand([2,1])*10);
-% 
-% x = zeros(1,n_samp);
-% x(orig_t_k) = orig_a_k;
+
+%% Stream of Diracs
+x = zeros(1,n_samp);
+x(orig_t_k) = orig_a_k;
 % figure
 % stem(x);
 % title('Orginal Stream of Diracs with K = 2');
 
 
+%% Daubechies scaling function
+phi = zeros(1,n_samp);
+[phi_T, psi_T, xval] = wavefun('dB4',6);
+phi(1:length(phi_T)) = phi_T;
+
+%% Sampling kernel
+y = x' .* phi';
+y = y(1:T:n_samp);
+
+s = C * y;
