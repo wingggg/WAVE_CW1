00f82c11db2a391481a9e040bb1bc796b57a6862
unknown <wsn12@eews506a-032.ic.ac.uk>
2016-03-27 21:32:52 +0100

    Q3 done

--- project_files_&_data/Q3.m
+++ project_files_&_data/Q3.m
@@ -1,5 +1,24 @@
 load('tau.mat');
-tau = tau;
+tau = tau';
+
+n_samp = 2048;
+T = 64;
 K = 2;
 
+% find annihilating filter
 h = annihilate_filter(tau, K);
+
+% find locations t_k
+t_k = roots(h);
+n_k = round(t_k*T);
+
+% find amplitude a_k
+vand = fliplr(vander(t_k))';
+a_k = vand\tau(1:K,:);
+
+x = zeros(1,n_samp);
+for i=1:length(t_k)
+    x(n_k(i)) = a_k(i);
+end
+figure
+stem(x);
--- project_files_&_data/annihilate_filter.m
+++ project_files_&_data/annihilate_filter.m
@@ -1,8 +1,9 @@
 function h = annihilate_filter(tau, K)
 % Program that, given a signal tau, find its annihinating filter h
 
-t1 = toeplitz(tau(K:end-1), tau(K:-1:1));
-t2 = tau(K+1:end);
-h = [1; t1\(-1*t2')];
+N = length(tau);
+t1 = toeplitz(tau(K:N-1), tau(K:-1:1));
+t2 = tau(K+1:N);
+h = [1; t1\(-1*t2)];
 
 end
