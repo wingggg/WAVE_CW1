ecb55e558dd2efa29ca73a8b8a8dbe5ac1616fea
unknown <wsn12@eews506a-032.ic.ac.uk>
2016-03-27 22:21:27 +0100

    Q4 start

---
+++ project_files_&_data/Q4.m
@@ -0,0 +1,14 @@
+n_samp = 2048;
+T = 64;
+K = 2;
+
+% orig_t_k = floor(rand([2,1])*n_samp/T)*T;
+% orig_a_k = floor(rand([2,1])*10);
+% 
+% x = zeros(1,n_samp);
+% x(orig_t_k) = orig_a_k;
+% figure
+% stem(x);
+% title('Orginal Stream of Diracs with K = 2');
+
+
